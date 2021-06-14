5e1ae1fc30e498394e97f72c4c919b9fa4eaa9a5
comet4 <sosu.cristian@gmail.com>
2021-06-14 20:16:44 +0300

    styling changes

--- public/popupWindow.js
+++ public/popupWindow.js
@@ -19,15 +19,19 @@ let popupWindow = document.getElementsByClassName('popupWindow')[0];
 
 let closeButton = document.getElementsByClassName('closeButton')[0];
 
+let openButton = document.querySelector('#helpButton');
+console.log(openButton);
 window.onload = function() {
 	popupWindow.style.display = 'block';
 };
 window.onclick = function(event) {
-	if ((event.target = popupWindow)) {
+	if (event.target == popupWindow && event.target != openButton) {
 		popupWindow.style.display = 'none';
 	}
 };
-
+openButton.onclick = function() {
+	popupWindow.style.display = 'block';
+};
 closeButton.onclick = function() {
 	popupWindow.style.display = 'none';
 };
